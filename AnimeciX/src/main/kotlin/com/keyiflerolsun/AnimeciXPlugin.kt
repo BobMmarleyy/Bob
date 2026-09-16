d5ec4f676cfed82ef14551a29545df3c30bd6d77
Bobmarley <onursiyhan07@gmail.com>
2026-09-16 04:11:04 +0300

    Add files via upload

---
+++ AnimeciX/src/main/kotlin/com/keyiflerolsun/AnimeciXPlugin.kt
@@ -0,0 +1,15 @@
+package com.keyiflerolsun
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class AnimeciXPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(AnimeciX())
+        registerExtractorAPI(TauVideo())
+        registerExtractorAPI(GoogleDriveExtractor())
+        registerExtractorAPI(SibNet())
+    }
+}
