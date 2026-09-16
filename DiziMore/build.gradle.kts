4a06e9c2fffd993a89de43804b363c3794e4fab7
Bobmarley <onursiyhan07@gmail.com>
2026-09-16 04:12:53 +0300

    Add files via upload

---
+++ DiziMore/build.gradle.kts
@@ -0,0 +1,19 @@
+// ! Bu araç @Kraptor123 tarafından | @kekikanime için yazılmıştır.
+version = 9
+
+cloudstream {
+    authors     = listOf("kraptor")
+    language    = "tr"
+    description = "Sadece Dizi izlemenin Tek Adresin yerli yabancı dizi izlemek için dizimore.com"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://dizimore.com&size=64"
+}
