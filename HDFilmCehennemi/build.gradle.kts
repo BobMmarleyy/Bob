aaa724a9146980cbbdda70a2ac1f62bb3b18deda
Bobmarley <onursiyhan07@gmail.com>
2026-09-16 04:13:58 +0300

    Add files via upload

---
+++ HDFilmCehennemi/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 34
+
+cloudstream {
+    authors     = listOf("hexated", "keyiflerolsun", "kraptor")
+    language    = "tr"
+    description = "Türkiye'nin en hızlı hd film izleme sitesi"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.com&sz=%size%"
+    isCrossPlatform = true
+}
